1498342413a35608355466ad6a664ac12bdc659d
doestack <doe035124@proton.me>
2026-05-19 07:19:59 +0100

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: env('DATABASE_URL'),
-    directUrl: env("DIRECT_URL"),
+    url: env('DIRECT_URL'),
   },
 });
